192b6a46c4894f278ce8b0083e7942730d2cb0c8
Ryan Pereira <ryan.dxb92@gmail.com>
2024-01-31 21:13:51 +0400

    Completed landing page

---
+++ app/src/app/site/layout.tsx
@@ -0,0 +1,21 @@
+import Navigation from "@/components/site/navigation";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+import React, { FC } from "react";
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout: FC<LayoutProps> = ({ children }) => {
+  return (
+    <ClerkProvider appearance={{ baseTheme: dark }}>
+      <main className="h-full">
+        <Navigation />
+        {children}
+      </main>
+    </ClerkProvider>
+  );
+};
+
+export default Layout;
